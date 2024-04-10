971f63de02de2343d96a025bb261b00879553925
shbhmj <shbhmkmrjha@gmail.com>
2024-04-10 19:31:58 +0530

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      <title>Shubham Reacts - A Technology LifeStyle Blog</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/180.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/32.png" />
+      <meta name="description" content="A Technology LifeStyle Blog" />
+      <meta name="author" content="Shubham Jha" />
+      <meta name="theme-color" content="#BF40BF" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:image" content="icons/512.png" />
+      <link rel="manifest" href="manifest.json" />
+    </>
+  );
+}
