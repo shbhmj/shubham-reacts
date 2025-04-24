ec5fe3731df005e73436f1a190f5536000323e1a
Shubham Home <agilevisiondev@gmail.com>
2025-04-24 06:44:46 +0530

    chore: update next.js version from 13.0.3 to ^15.3.1 in package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,7 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            {children}
-          </div>
-        </div>
+        <main className="py-4">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -20,27 +20,44 @@ const Page = () => {
   const blogs = use(getInitialBlogs());
 
   return (
-    <div>
-      <h2 className="sr-only">Blogs</h2>
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {blogs.map((blog) => (
-          <Link key={blog.slug} href={`/blogs/${blog.slug}`} className="group">
-            <div className="relative aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-              <Image
-                fill
-                src={blog.coverImage}
-                alt={""}
-                className="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 className="mt-4 text-sm text-gray-700">{blog.title}</h3>
-            <p className="mt-1 text-md font-medium text-gray-900">
-              {shortify(blog.description, 100)}
-            </p>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      <header className="sticky z-20 top-0 shadow bg-white">
+        <div className="mx-auto py-2 max-w-2xl text-left lg:max-w-7xl lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 px-4 md:px-0  sm:text-5xl">
+            Shubham Reacts
+          </h1>
+          <p className="mt-2 max-w-7xl hidden lg:block text-left text-sm sm:text-lg lg:leading-tight text-gray-600 ">
+            Dive into the convergence of tech and culture—unmasking digital
+            revolutions, dispelling hacker myths, probing AI ethics, shattering
+            conventions with pointed analysis, sly wit, and boundless curiosity
+          </p>
+        </div>
+      </header>
+      <main className="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {blogs.map((blog) => (
+            <Link
+              key={blog.slug}
+              href={`/blogs/${blog.slug}`}
+              className="group"
+            >
+              <div className="relative aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <Image
+                  fill
+                  src={blog.coverImage}
+                  alt={""}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{blog.title}</h3>
+              <p className="mt-1 text-md font-medium text-gray-900">
+                {shortify(blog.description, 100)}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
   );
 };
 
